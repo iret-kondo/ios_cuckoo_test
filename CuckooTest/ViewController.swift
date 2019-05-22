7db9dc9cb717b00ef46ba135e4229096a8c6c468
近藤 寛志 <kondo@cloudpack.jp>
2019-05-22 11:50:54 +0900

    Initial Commit

---
+++ CuckooTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuckooTest
+//
+//  Created by 近藤 寛志 on 2019/05/22.
+//  Copyright © 2019 iret, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
